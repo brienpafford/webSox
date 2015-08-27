6a6d4aaf87c290b9689c9b36e36a6fe317b6c949
Brien Pafford <bcpaff@gmail.com>
2015-08-27 15:34:10 -0500

    Append messages from input box to DOM, open multiple browsers to see synchronous magic

--- server/app.js
+++ server/app.js
@@ -18,6 +18,27 @@ var server = app.listen(3000, function () {
 
 var io = socketio(server);
 
-io.on('connection', function(socket) {
-	console.log('Client connected:', socket.id);
+io.on('connection', function (socket) {
+  console.log('Client connected:', socket.id);
+
+// On disconnect
+
+  socket.on('disconnect', function () {
+    console.log('Client disconnected:', socket.id);
+  });
+
+// Message Display
+
+  socket.on('chatMessage', function (msg) {
+  	console.log('Chat Message received:', msg);
+
+ 	// Sending message to one, everyone but one, and everyone 
+
+  // socket.emit('chatMessage', {toOne: msg});
+  
+  // socket.broadcast.emit('chatMessage', {toOthers: msg});	
+  
+  io.emit('chatMessage', msg);
+});
+
 });
