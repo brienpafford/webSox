99c658664203997f6f10900d266fd5b302a82089
Brien Pafford <bcpaff@gmail.com>
2015-08-27 14:35:37 -0500

    installed client side of socket.io

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 'use strict';
 
 var express 		= require('express');
+var socketio		= require('socket.io');
 
 var app 				= express();
 
 
 app.use(express.static('client'));
 
-app.listen(3000, function () {
+
+var server = app.listen(3000, function () {
 	console.log('Server listening on port 3000');
 });
+
+// We have to pass in socket.io into the server. 
+//So we just set it to server, then pass it in 
+
+socketio(server);
